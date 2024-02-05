a6532c819f0f52254d58503bf959d6cf7239c0bf
CALVIN MAÑALAC <calvin.manalac@hoolisoftware.com>
2024-02-05 15:40:31 +0800

    use imports

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,30 @@
+import profile from '../../src/assets/images/profile.jpg'
+import toga from '../../src/assets/images/toga.jpg'
+import hooli from '../../src/assets/images/hooli.jpeg'
+import rp from '../../src/assets/images/rp-logo.png'
+import mySql from '../../src/assets/images/mysql-logo.png'
+import js from '../../src/assets/images/js-logo.png'
+import java from '../../src/assets/images/java-logo.png'
+import mui from '../../src/assets/images/mui-logo.webp'
+import reactQuery from '../../src/assets/images/react-query.png'
+import react from '../../src/assets/images/react-logo.webp'
+import typeScript from '../../src/assets/images/typescript.png'
+import spring from '../../src/assets/images/spring-logo.png'
+import tailwind from '../assets/images/tailwind-logo.png'
+import node from '../../src/assets/images/node-logo.png'
+
+import resume from '../../public/cv_calvs.pdf'
+import bit from '../../public/js-bit.png'
+import pl from '../../public/pl.pdf'
+import dl from '../../public/dl.pdf'
+import aws from '../../public/aws.pdf'
+import blending from '../../public/blending.pdf'
+import gmaps from '../../public/googlemaps.pdf'
+import iT from '../../public/it-carrer.jpg'
+import journey from '../../public/journey.png'
+import jsEs6 from '../../public/js-es6.pdf'
+import keeping from '../../public/keeping-htm.pdf'
+
 type logoType = {
     mySql: string
     js: string
@@ -11,21 +38,21 @@ type logoType = {
     node: string
 }
 export const images = {
-    profile: '../../src/assets/images/profile.jpg',
-    toga: '../../src/assets/images/toga.jpg',
-    hooli: '../../src/assets/images/hooli.jpeg',
-    rp: '../../src/assets/images/rp-logo.png',
+    profile,
+    toga,
+    hooli,
+    rp,
     logo: {
-        mySql: '../../src/assets/images/mysql-logo.png',
-        js: '../../src/assets/images/js-logo.png',
-        java: '../../src/assets/images/java-logo.png',
-        mui: '../../src/assets/images/mui-logo.webp',
-        reactQuery: '../../src/assets/images/react-query.png',
-        react: '../../src/assets/images/react-logo.webp',
-        typeScript: '../../src/assets/images/typescript.png',
-        spring: '../../src/assets/images/spring-logo.png',
-        tailwind: '../../src/assets/images/Tailwind_CSS_Logo.svg.png',
-        node: '../../src/assets/images/node-logo.png',
+        mySql,
+        js,
+        java,
+        mui,
+        reactQuery,
+        react,
+        typeScript,
+        spring,
+        tailwind,
+        node,
     } as logoType,
 }
 export const logoAsArray = Object.keys(images.logo).map(
@@ -44,18 +71,18 @@ type certs = {
     keeping: string
 }
 export const docs = {
-    resume: '../../public/cv_calvs.pdf',
+    resume,
     certificates: {
-        bit: '../../public/js-bit.png',
-        pl: '../../public/pl.pdf',
-        dl: '../../public/dl.pdf',
-        aws: '../../public/aws.pdf',
-        blending: '../../public/blending.pdf',
-        gmaps: '../../public/googlemaps.pdf',
-        iT: '../../public/it-career.JPG/',
-        journey: '../../public/journey.png',
-        jsEs6: '../../public/js-es6.pdf',
-        keeping: '../../public/keeping-htm.pdf',
+        bit,
+        pl,
+        dl,
+        aws,
+        blending,
+        gmaps,
+        iT,
+        journey,
+        jsEs6,
+        keeping,
     } as certs,
 }
 export const certificatesArray = Object.keys(docs.certificates).map(
